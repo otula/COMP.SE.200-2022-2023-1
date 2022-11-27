a008f54e73e28ec432f766624e54d4608df1a27f
joonas175 <36733885+joonas175@users.noreply.github.com>
2022-11-27 11:21:05 +0200

    Add tests to get, isArrayLike and isBoolean

--- src/__tests__/isArrayLike.test.js
+++ src/__tests__/isArrayLike.test.js
@@ -1,1 +1,29 @@
 /** @author Joonas Salojärvi */
+
+import {expect, test} from '@jest/globals';
+import isArrayLike from '../isArrayLike';
+
+test('Array should be array like', () => {
+    const resolved = isArrayLike([1,2,3]);
+    expect(resolved).toEqual(true);
+})
+
+test('Object should not be array like', () => {
+    const resolved = isArrayLike({a: 1, b: 2});
+    expect(resolved).toEqual(false);
+})
+
+test('String should be array like', () => {
+    const resolved = isArrayLike('TEST STRING');
+    expect(resolved).toEqual(true);
+})
+
+test('Function should not be array like', () => {
+    const resolved = isArrayLike(isArrayLike);
+    expect(resolved).toEqual(false);
+})
+
+test('Anonymous function should not be array like', () => {
+    const resolved = isArrayLike(() => true);
+    expect(resolved).toEqual(false);
+})
--- src/__tests__/isBoolean.test.js
+++ src/__tests__/isBoolean.test.js
@@ -1,1 +1,45 @@
 /** @author Joonas Salojärvi */
+
+import {expect, test} from '@jest/globals';
+import isBoolean from '../isBoolean';
+
+test('New Boolean() should be boolean', () => {
+    const resolved = isBoolean(new Boolean());
+    expect(resolved).toEqual(true);
+})
+
+test('New Boolean(false) should be boolean', () => {
+    const resolved = isBoolean(new Boolean(false));
+    expect(resolved).toEqual(true);
+})
+
+test('true should be boolean', () => {
+    const resolved = isBoolean(true);
+    expect(resolved).toEqual(true);
+})
+
+test('false should be boolean', () => {
+    const resolved = isBoolean(false);
+    expect(resolved).toEqual(true);
+})
+
+test('Object should not be boolean', () => {
+    const obj = { asd: 'asd' }
+    const resolved = isBoolean(obj);
+    expect(resolved).toEqual(false);
+})
+
+test('Number should not be boolean', () => {
+    const resolved = isBoolean(new Number(10));
+    expect(resolved).toEqual(false);
+})
+
+test('Null should not be boolean', () => {
+    const resolved = isBoolean(null);
+    expect(resolved).toEqual(false);
+})
+
+test('Undefined should not be boolean', () => {
+    const resolved = isBoolean(undefined);
+    expect(resolved).toEqual(false);
+})

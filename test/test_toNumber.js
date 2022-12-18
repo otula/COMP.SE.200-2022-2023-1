7d3752e3a2c3f5c55c76fe9dc7a71014d51d3bd0
kkemppi <kemppi.mikko@gmail.com>
2022-12-18 19:14:50 +0200

    toNumber initial tests

--- test/test_toNumber.js
+++ test/test_toNumber.js
@@ -6,4 +6,25 @@ describe('#toNumber', function(){
         expect(toNumber(1)).to.equal(1)
         expect(toNumber(1.0001)).to.equal(1.0001)
     });
+
+    it('number strings return correctly', function(){
+        expect(toNumber("1")).to.equal(1)
+        expect(toNumber("1.0001")).to.equal(1.0001)
+    });
+
+    it('infinity should return infinity', function(){
+        expect(toNumber(Infinity)).to.equal(Infinity)
+    });
+
+    it('minimum should return "5e-324"', function(){
+        expect(toNumber(Number.MIN_VALUE)).to.equal(5e-324)
+    });
+
+    it('string should return NAN', function(){
+        var isnan = Number.isNaN(toNumber('abc'))
+        expect(isnan)
+    });
+
+    // TODO: Binary, octal and hexadecimal tests
+
 });

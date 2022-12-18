917d3643085a042aea2600b209bea9514d34cb7e
kkemppi <kemppi.mikko@gmail.com>
2022-12-18 18:32:41 +0200

    capitalize, filter, get and toNumber initial implementation

--- test/test_filter.js
+++ test/test_filter.js
@@ -2,6 +2,10 @@ import { expect } from 'chai';
 import filter from '../src/filter.js';
 
 describe('#filter', function(){
-    it('should capitalize the first letter', function(){
+    it('should filter results that do not fulfill the predicate', function(){
+        const testList = [{'name':'test1','pass': true}, {'name':'test2', 'pass': false}];
+        const expected = [{'name':'test1','pass': true}];
+        
+        expect(filter(testList, ({pass}) => pass)).to.equal(expected);
     });
 });
--- test/test_get.js
+++ test/test_get.js
@@ -2,6 +2,14 @@ import { expect } from 'chai';
 import get from '../src/get.js';
 
 describe('#get', function(){
-    it('should capitalize the first letter', function(){
+    it('should return the value at the end of the path', function(){
+        const testObj = { 'a': [{ 'b': { 'c': 3 } }] };
+        const expected = 3;
+        expect(get(testObj, 'a[0].b.c')).to.equal(expected);
+    });
+    it('should defaultvalue when path does not exist', function(){
+        const testObj = { 'a': [{ 'b': { 'c': 3 } }] };
+        const expected = false;
+        expect(get(testObj, 'a.b.c', false)).to.equal(expected);
     });
 });
